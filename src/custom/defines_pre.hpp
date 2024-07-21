4ffa4d75c0ec7ffe1f5d38688741a43af22c443f
Umay7 <umar.mustaqim182@gmail.com>
2024-07-22 01:53:36 +0700

    hmm

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -8,7 +8,8 @@
  * rAthena configuration file (http://rathena.org)
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
-  #define PACKETVER 20180530
+  //#define PACKETVER 20180530
+  #define PACKETVER 20200401
 
 
 #endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
